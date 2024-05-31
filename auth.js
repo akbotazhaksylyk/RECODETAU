92dfc24883b14e8d15612a753a21202c8fef7ff5
Akbota Zhaksylyk <138726392+akbotazhaksylyk@users.noreply.github.com>
2024-05-31 19:02:17 +0500

    from bcrypt to bcryptjs

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('./models/User');
 // Import bcrypt if you want to hash passwords
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 router.post('/register', async (req, res) => {
     try {
